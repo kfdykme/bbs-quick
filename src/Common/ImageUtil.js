43d8fb105eeabb1d551204d2c933549333e2ec0c
kfdykme <wim.k.f@live.com>
2018-11-02 05:18:15 +0800

    在ＩｍａｇｅＰｉｃｋＢａｒ中可以正常打开Other/Image了

--- src/Common/ImageUtil.js
+++ src/Common/ImageUtil.js
@@ -1,5 +1,11 @@
 import router from "@system.router"
 
+/**
+ * @method ViewImage
+ * @param {string} uri
+ * @param {array} images
+ * @param {string} type
+ */
 function ViewImage(uri,images,type){
 
     if(type == null) type == 'view'
